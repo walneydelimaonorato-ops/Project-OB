a4a6a9f09104353b0f21ca922d7d1aa73234ee04
Bread Boyo <walneydelimaonorato@gmail.com>
2026-04-21 10:54:33 -0300

    Update Actions_Script.gd

--- Assets/Player/Scripts/Actions_Script.gd
+++ Assets/Player/Scripts/Actions_Script.gd
@@ -10,13 +10,14 @@ func Action_Alternative():
 	StatsMan.Geneneral_Interaction(%Ray2, "Iteraction")
 	StatsMan.General_Animations.play("Tools_Anims/LeftHand_Interact")
 
+# Takes in the direction of the Action, matches what tool is equipped in what hand snd executes the action
 func Action_Primary(Direction):
 	match Direction:
 		"Right":
 			match PlayerRes.data.Inv_ToolR_Equiped:
 				"null":
 					StatsMan.HandGun_Shoot.play()
-					SignalBus.emit_signal("request_damage", PlayerRes.data.Tool_ID["AssaultRifle"]["damage"])
+					SignalBus.emit_signal("request_damage", PlayerRes.data.Tool_ID["AssaultRifle"]["damage"]) # Signals to the entity receiving damage the damage of the current tool equipped
 					StatsMan.Geneneral_Interaction(%Ray1, "Take_Damage")
 				"HandGun":
 					StatsMan.General_Animations.play("Tools_Anims/HandGun_Shoot")
