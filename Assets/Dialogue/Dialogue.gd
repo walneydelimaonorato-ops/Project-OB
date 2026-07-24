9ad6a54d71a2725666a5f8fa51ceec4a63274887
Bread Boyo <walneydelimaonorato@gmail.com>
2026-07-24 20:55:41 -0300

    Code Update

--- Assets/Dialogue/Dialogue.gd
+++ Assets/Dialogue/Dialogue.gd
@@ -4,17 +4,66 @@ var Library = {
 	NPC_TEST_INTRODUCTION = {
 		BOOK1 = {
 			"LINE": "This is a test introduction",
-			"VOICE": "Nothing",
-			"PROCEED": "END"
+			"VOICE": "res://Assets/Dialogue/TEST NPC/NPC_TEST_INTRODUCTION_001.mp3",
+			"PROCEED": "NEXT",
+			"NEXT": "BOOK2",
+		},
+		BOOK2 = {
+			"LINE": "This is an example of the next line",
+			"VOICE": "res://Assets/Dialogue/TEST NPC/NPC_TEST_INTRODUCTION_001.mp3",
+			"PROCEED": "NEXT",
+			"NEXT": "BOOK3",
+		},
+		BOOK3 = {
+			"LINE": "Pretty neat, right?",
+			"VOICE": "res://Assets/Dialogue/TEST NPC/NPC_TEST_INTRODUCTION_001.mp3",
+			"PROCEED": "SPLIT",
+			"ADRS": "",
+			"PASS A": "BOOK4A",
+			"PASS B": "BOOK4B",
+		},
+		BOOK4A = {
+			"LINE": "Awesome, I know",
+			"VOICE": "res://Assets/Dialogue/TEST NPC/NPC_TEST_INTRODUCTION_001.mp3",
+			"PROCEED": "NEXT",
+			"NEXT": "BOOK5A",
+		},
+		BOOK4B = {
+			"LINE": "Really? It might not look like much",
+			"VOICE": "res://Assets/Dialogue/TEST NPC/NPC_TEST_INTRODUCTION_001.mp3",
+			"PROCEED": "NEXT",
+			"NEXT": "BOOK5B",
+		},
+		BOOK5A = {
+			"LINE": "It took SO MUCH to put together",
+			"VOICE": "res://Assets/Dialogue/TEST NPC/NPC_TEST_INTRODUCTION_001.mp3",
+			"PROCEED": "NEXT",
+			"NEXT": "BOOK6",
+		},
+		BOOK5B = {
+			"LINE": "But this spent 3 days in the oven!",
+			"VOICE": "res://Assets/Dialogue/TEST NPC/NPC_TEST_INTRODUCTION_001.mp3",
+			"PROCEED": "NEXT",
+			"NEXT": "BOOK6",
+		},
+		BOOK6 = {
+			"LINE": "Im probably taking too much of your time",
+			"VOICE": "res://Assets/Dialogue/TEST NPC/NPC_TEST_INTRODUCTION_001.mp3",
+			"PROCEED": "NEXT",
+			"NEXT": "BOOK7",
+		},
+		BOOK7 = {
+			"LINE": "...",
+			"VOICE": "res://Assets/Dialogue/TEST NPC/NPC_TEST_INTRODUCTION_001.mp3",
+			"PROCEED": "END",
+			"NEXT": ""
 		}
 	}
 }
 
-
-#var NPC_TEST_INTRODUCTION = {
-	#BOOK1 = {
-		#"LINE": "This is a test introduction",
-		#"VOICE": "Nothing",
-		#"PROCEED": "END"
-	#}
-#}
+		#BOOK2 = {
+			#"LINE": "This is an example of the next line",
+			#"VOICE": "res://Assets/Dialogue/TEST NPC/NPC_TEST_INTRODUCTION_001.mp3",
+			#"PROCEED": "NEXT",
+			#"NEXT": "BOOK3",
+		#},
--- Assets/Player/Scripts/Managers/Interaction Manager/Dialogue Manager.gd
+++ Assets/Player/Scripts/Managers/Interaction Manager/Dialogue Manager.gd
@@ -10,23 +10,24 @@ func _ready() -> void:
 func Diag_Play(Param1: String, Param2: String):
 	Local_Storage_Param1 = Param1
 	Local_Storage_Param2 = Param2
+	print_rich("[color=blue]", Local_Storage_Param1, " ", Local_Storage_Param2, "[/color]")
 	
 	%"Dialogue Menu".visible = true
 	#%"Dialogue Container".self_modulate.a = lerp(%"Dialogue Container".self_modulate.a, 0, 1)
 	
 	%"Current Book".text = Global.Dialogue.Library[Param1][Param2]["LINE"]
-	#%"Current Voice".stream = Global.Dialogue[Param1][Param2]["VOICE"]
+	%"Current Voice".stream = load(Global.Dialogue.Library[Param1][Param2]["VOICE"])
 	%"Current Voice".playing = true
 
 func _current_voice_finished() -> void:
 	Proceed_Sytem()
 
 func Proceed_Sytem():
-	match Global.Dialogue[Local_Storage_Param1][Local_Storage_Param2]["PROCEED"]:
+	match Global.Dialogue.Library[Local_Storage_Param1][Local_Storage_Param2]["PROCEED"]:
 		"NEXT":
-			pass
+			SignalBus.emit_signal("Interaction_Manager_Request", "Dialogue", "", "NPC_TEST_INTRODUCTION", Global.Dialogue.Library[Local_Storage_Param1][Local_Storage_Param2]["NEXT"])
 		"SPLIT":
 			pass
 		"END":
-			%"Dialogue Backdrop".visible = false
+			%"Dialogue Menu".visible = false
 			return
